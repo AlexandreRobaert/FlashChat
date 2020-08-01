c96d6e835a851558a7ca2a2fdfc5e102d77b1d13
Alexandre Robaert <robaert.developer@icloud.com>
2020-08-01 14:57:56 -0300

    Initial Commit

---
+++ FlashChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Alexandre Robaert on 01/08/20.
+//  Copyright © 2020 Alexandre Robaert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
