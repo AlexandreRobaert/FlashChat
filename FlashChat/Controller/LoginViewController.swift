40a1a176c8d516a4779876aa1acb2a83a8d42de3
Alexandre Robaert <robaert.developer@icloud.com>
2020-08-03 02:02:22 -0300

    Commit Inicial com StoryBoard pronto

---
+++ FlashChat/Controller/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  FlashChat
+//
+//  Created by Alexandre Robaert on 01/08/20.
+//  Copyright © 2020 Alexandre Robaert. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func loginPressed(_ sender: UIButton) {
+        
+    }
+}
---
+++ FlashChat/Controller/RegisterViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterViewController.swift
+//  FlashChat
+//
+//  Created by Alexandre Robaert on 01/08/20.
+//  Copyright © 2020 Alexandre Robaert. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func registerPressed(_ sender: UIButton) {
+        
+    }
+}
